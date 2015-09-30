624d53a942fb39cfbb509905ef4f7d6274670ffa
fenghuangzhan <292251753@qq.com>
2015-09-30 21:55:52 +0800

    建立项目

---
+++ guet_learngit/guet_learngit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  guet_learngit
+//
+//  Created by Zuo Ruirui on 15/9/30.
+//  Copyright © 2015年 GUET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ guet_learngit/guet_learngit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  guet_learngit
+//
+//  Created by Zuo Ruirui on 15/9/30.
+//  Copyright © 2015年 GUET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
